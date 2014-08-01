23736d3b64dd639a9fe1705a7cada0d6c3569643
Prateek Saxena <prtksxna@gmail.com>
2014-08-01 15:48:20 +0530

    Jump to FAQ according to location hash
    - Added named anchors for all questions
    - Open current answer on DOM ready
    
    Change-Id: I43bdec06eb163416c24132d66c53a7c201212c45

--- source/javascripts/faq.js
+++ source/javascripts/faq.js
@@ -3,4 +3,8 @@ $( function () {
 	$( '.faq' ).click( function () {
 		$( this ).next().slideToggle();
 	} );
+	if ( window.location.hash !== '' ) {
+		var hash = window.location.hash.split( '#' )[1];
+		$( 'a[name=' + hash ).next().next().slideToggle();
+	}
 } );
