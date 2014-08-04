25737647e53c97b57dfe571f0423795482b6a271
Prateek Saxena <prtksxna@gmail.com>
2014-08-04 12:01:20 +0530

    Fix selector in FAQ
    Fixed the FAQ stuff in Firefox
    
    Change-Id: I59ec225925bb99bfed922566490b08ed462588bb

--- source/javascripts/faq.js
+++ source/javascripts/faq.js
@@ -6,7 +6,7 @@ $( function () {
 	} );
 	if ( window.location.hash !== '' ) {
 		var hash = window.location.hash.split( '#' )[1];
-		$( 'a[name=' + hash ).next().toggleClass( 'open' );
-		$( 'a[name=' + hash ).next().next().slideToggle();
+		$( 'a[name=' + hash + ']' ).next().toggleClass( 'open' );
+		$( 'a[name=' + hash + ']' ).next().next().slideToggle();
 	}
 } );
