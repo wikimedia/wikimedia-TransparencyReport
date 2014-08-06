54f1e57cd6444754b0c5ed03e8a5c43d57f96e0d
Prateek Saxena <prtksxna@gmail.com>
2014-08-06 16:23:04 +0530

    FAQ: Use CSS transitions intead of jQuery.animate
    Change-Id: I37279b0fa12b3e5b45c9a4b2640d292aea06a36b

--- source/javascripts/faq.js
+++ source/javascripts/faq.js
@@ -1,12 +1,11 @@
 $( function () {
-	$( '.answer').hide();
 	$( '.question' ).click( function () {
 		$( this ).toggleClass( 'open' );
-		$( this ).next().slideToggle();
+		$( this ).next().toggleClass( 'open' );
 	} );
 	if ( window.location.hash !== '' ) {
 		var hash = window.location.hash.split( '#' )[1];
+		$( 'a[name=' + hash + ']' ).next().next().toggleClass( 'open' );
 		$( 'a[name=' + hash + ']' ).next().toggleClass( 'open' );
-		$( 'a[name=' + hash + ']' ).next().next().slideToggle();
 	}
 } );
